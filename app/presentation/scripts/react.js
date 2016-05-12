9db09fe03daa9dad00bf05b2e90cce0aa14c7e1a
wklm <wojtekkulma@gmail.com>
2016-05-12 18:13:48 +0200

    react, socket.io init

---
+++ app/presentation/scripts/react.js
@@ -0,0 +1,22 @@
+console.log("ello!");
+
+"use strict";
+//var socket = io.connect('localhost:3000' );
+
+
+var Root = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p> lalal </p>
+      </div>
+    )
+  }
+  });
+
+ReactDOM.render(
+    <Root />,
+    document.getElementById('react')
+);
+
+
