dcbb02f8411744fb89afac9d37b841550aa74310
wklm <wojtekkulma@gmail.com>
2016-05-13 12:32:36 +0200

    presentation_layer: grid structure

--- app/presentation/scripts/react.js
+++ app/presentation/scripts/react.js
@@ -1,22 +1,80 @@
-console.log("ello!");
-
 "use strict";
 //var socket = io.connect('localhost:3000' );
 
+var Row = React.createClass({
+  trigger: function(col, row) {
+    console.log("clicked column: " + col + ", row: " + row );
+  },
+  render: function () {
 
-var Root = React.createClass({
-  render: function() {
+
+    let cells = this.props.row.x.map(cell => {
+      return (
+        <div
+          key={Math.random()}
+          onClick={() => this.trigger(this.props.row.y, cell)}
+        >
+          {cell}
+        </div>
+      )
+    })
+
     return (
       <div>
-        <p> lalal </p>
+        {cells}
       </div>
     )
   }
-  });
+});
+
+
+var Grid = React.createClass({
+  getInitialState: function () {
+    return {
+      matrix: new Array()
+    }
+  },
+
+  componentWillMount: function () {
+    for (let i = 0; i < 7;) { // columns
+      this.state.matrix[i] = {
+        x:  Array.apply(null, {length: 6}).map(Number.call, Number), // rows
+        y: i++
+      }
+    }
+  },
+
+  render: function () {
+    let rows = this.state.matrix.map(row => {
+      return (
+        <td key={row.y}>
+          <Row row={row}/>
+        </td>
+      )
+    })
+    return (
+      <table>
+        {rows}
+      </table>
+    )
+  }
+});
+
+
+var Root = React.createClass({
+  getInitialState: function () {
+    return {}
+  },
+  render: function () {
+    return (
+      <Grid />
+    )
+  }
+});
 
 ReactDOM.render(
-    <Root />,
-    document.getElementById('react')
+  <Root />,
+  document.getElementById('react')
 );
 
 
